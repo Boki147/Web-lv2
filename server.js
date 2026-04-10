7b1badc00145ea5e6b34b35bdb648d75be99e4b0
Boki147 <kelic.bozidar7@gmail.com>
2026-04-10 12:48:26 +0200

    Updated project (EJS + fixes)

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static('public'));
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 app.get('/', (req, res) => {
-res.send('Pozdrav sa Railway servera!');
+  res.redirect('/slike');
 });
+
+// GALERIJA 
+app.get('/slike', (req, res) => {
+  const dataPath = path.join(__dirname, 'images.json');
+  const images = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
+  res.render('slike', { images });
+});
+
+
 app.listen(PORT, () => {
-console.log('Server pokrenut na portu ${PORT}');
+  console.log(`Server pokrenut na portu ${PORT}`);
 });
